ae1f3ddc43f8361058195dddbe3dcbf0b59ff50d
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2025-10-13 05:48:16 -0700

    Enhanced user  experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayRecipe(response) {
-    new Typewriter('#recipe', {
+    new Typewriter('#result', {
   strings: response.data.answer,
   autoStart: true,
   delay: 1,
@@ -13,6 +13,9 @@ function generateRecipe(event) {
     let prompt=`Create a detailed recipe for ${inputElement.value}. Include ingredients, measurements, and step-by-step instructions.`;
     let context="you are a professional chef. generate clear, step by step recipe instructions based on the user's input. be concise and easy to follow. write in html(do not include <html>) format and break lines between sections. include a title, ingredients list, and numbered steps. avoid unnecessary commentary. start with a title, then ingredients, then instructions. write in simple language/English." ;
     let apiurl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`
+
+    let recipeElement = document.querySelector("#result");
+    recipeElement.innerHTML = "Generating recipe...";
 
     axios.get(apiurl).then(displayRecipe);
 }
